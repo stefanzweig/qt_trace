8dbc4b1555e4c7bc6570a53fde51cca06144d402
DESKTOP-NBM6U6D\Administrator <liuhaijiang@gmail.com>
2024-06-06 16:27:45 +0800

    new subscriber

---
+++ topicData/ZoneMasterCanMessageDataSubscriber.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "topicData/ZoneMasterDataPubSubTypes.h"
+#include "topicData/ZoneMasterCanMessageDataListener.h"
+
+#include <chrono>
+#include <thread>
+
+#include <fastdds/dds/domain/DomainParticipant.hpp>
+#include <fastdds/dds/domain/DomainParticipantFactory.hpp>
+#include <fastdds/dds/subscriber/DataReader.hpp>
+#include <fastdds/dds/subscriber/DataReaderListener.hpp>
+#include <fastdds/dds/subscriber/qos/DataReaderQos.hpp>
+#include <fastdds/dds/subscriber/SampleInfo.hpp>
+#include <fastdds/dds/subscriber/Subscriber.hpp>
+#include <fastdds/dds/topic/TypeSupport.hpp>
+
+using namespace eprosima::fastdds::dds;
+
+class ZoneMasterCanMessageDataSubscriber
+{
+private:
+
+    DomainParticipant* participant_;
+
+    Subscriber* subscriber_;
+
+    DataReader* reader_;
+
+    Topic* topic_;
+
+    TypeSupport type_;
+public:
+    ZoneMasterCanMessageDataSubscriber()
+        : participant_(nullptr)
+        , subscriber_(nullptr)
+        , topic_(nullptr)
+        , reader_(nullptr)
+        , type_(new canMessagesPubSubType()) // this should be matched.
+    {
+    }
+
+    virtual ~ZoneMasterCanMessageDataSubscriber()
+    {
+        if (reader_ != nullptr)
+        {
+            subscriber_->delete_datareader(reader_);
+        }
+        if (topic_ != nullptr)
+        {
+            participant_->delete_topic(topic_);
+        }
+        if (subscriber_ != nullptr)
+        {
+            participant_->delete_subscriber(subscriber_);
+        }
+        DomainParticipantFactory::get_instance()->delete_participant(participant_);
+    }
+
+    bool init();
+
+    void run(uint32_t samples);
+
+};
+
