bb7d5332c66f2ea3fb676498e766217c05f0d837
Stefan Zweig <stefan.zweig@gmail.com>
2025-03-17 09:42:12 +0800

    res

--- xmake.lua
+++ xmake.lua
@@ -13,6 +13,7 @@ target("zone-tracer")
     add_files("topicData/ZoneMasterLinParserDataSubscriber.cpp")
     add_files("topicData/ZoneMasterSomeipDataSubscriber.cpp")
 
+    add_files("QtWidgetsApplication1.qrc")
     add_files("columnfilter.h")
     add_files("dialog/configdialog.h")
     add_files("LineEditFilter.h")
@@ -52,77 +53,9 @@ target("zone-tracer")
     add_includedirs("/home/stefliu/tools/Qt5.14.2/5.14.2/gcc_64/include/QtGui/")
     add_includedirs("/usr/include/libdrm")
     add_includedirs("/home/stefliu/tools/Qt5.14.2/5.14.2/gcc_64/mkspecs/linux-g++")
+
     add_links("Qt5Widgets", "Qt5Core", "Qt5Gui")
     add_links("fastrtps", "fastcdr")
     add_linkdirs("/home/stefliu/tools/Qt5.14.2/5.14.2/gcc_64/lib/")
     add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
-
---
--- If you want to known more usage about xmake, please see https://xmake.io
---
--- ## FAQ
---
--- You can enter the project directory firstly before building project.
---
---   $ cd projectdir
---
--- 1. How to build project?
---
---   $ xmake
---
--- 2. How to configure project?
---
---   $ xmake f -p [macosx|linux|iphoneos ..] -a [x86_64|i386|arm64 ..] -m [debug|release]
---
--- 3. Where is the build output directory?
---
---   The default output directory is `./build` and you can configure the output directory.
---
---   $ xmake f -o outputdir
---   $ xmake
---
--- 4. How to run and debug target after building project?
---
---   $ xmake run [targetname]
---   $ xmake run -d [targetname]
---
--- 5. How to install target to the system directory or other output directory?
---
---   $ xmake install
---   $ xmake install -o installdir
---
--- 6. Add some frequently-used compilation flags in xmake.lua
---
--- @code
---    -- add debug and release modes
---    add_rules("mode.debug", "mode.release")
---
---    -- add macro definition
---    add_defines("NDEBUG", "_GNU_SOURCE=1")
---
---    -- set warning all as error
---    set_warnings("all", "error")
---
---    -- set language: c99, c++11
---    set_languages("c99", "c++11")
---
---    -- set optimization: none, faster, fastest, smallest
---    set_optimize("fastest")
---
---    -- add include search directories
---    add_includedirs("/usr/include", "/usr/local/include")
---
---    -- add link libraries and search directories
---    add_links("tbox")
---    add_linkdirs("/usr/local/lib", "/usr/lib")
---
---    -- add system link libraries
---    add_syslinks("z", "pthread")
---
---    -- add compilation and link flags
---    add_cxflags("-stdnolib", "-fno-strict-aliasing")
---    add_ldflags("-L/usr/local/lib", "-lpthread", {force = true})
---
--- @endcode
---
 
